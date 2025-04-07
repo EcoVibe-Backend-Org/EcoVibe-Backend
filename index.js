d0628484ba392bec381fb5b3c2ca93a6f2c56c56
omar-leithym <124714854+omar-leithym@users.noreply.github.com>
2025-04-07 19:23:46 +0200

    added authentication

--- index.js
+++ index.js
@@ -9,6 +9,18 @@ const openai = new OpenAI({ apiKey: process.env.OPENAI_KEY });
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use("/api/users", require("./routers/userRoutes"))
+const mongoose = require("mongoose")
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+.then((result) => {
+    app.listen(3000, () => {
+        console.log("server is running")
+    })
+}).catch((err) => {
+    console.log(err)
+})
+
 
 app.post("/analyze-image", async (req, res) => {
   try {
@@ -79,8 +91,9 @@ app.get("/health", (req, res) => {
     timestamp: new Date().toISOString(),
   });
 });
+
 // Start the server
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Backend running on http://localhost:${PORT}`);
-});
+// const PORT = 3000;
+// app.listen(PORT, () => {
+//   console.log(`Backend running on http://localhost:${PORT}`);
+// });

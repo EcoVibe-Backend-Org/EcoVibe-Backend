7f126bdcd921bb50e426a80712905dd8eeac0456
omar-leithym <124714854+omar-leithym@users.noreply.github.com>
2025-04-30 03:16:59 +0300

    .

--- index.js
+++ index.js
@@ -16,9 +16,6 @@ app.use("/api/comments", require("./routers/CommentRoutes"))
 app.use("/api/posts", require("./routers/postRoutes"))
 const mongoose = require("mongoose")
 
-const path = require('path');
-app.use('/uploads', express.static('uploads'));
-
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 .then((result) => {
     app.listen(3000, () => {

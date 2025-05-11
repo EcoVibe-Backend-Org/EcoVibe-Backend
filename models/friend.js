4ed63b786a6aa6e8b04d5dd3ed003f84509918fd
omar-leithym <124714854+omar-leithym@users.noreply.github.com>
2025-05-11 11:38:42 +0300

    added friends

---
+++ models/friend.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const friendSchema = mongoose.Schema({
+  requester: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  recipient: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  status: { type: String, enum: ['pending', 'accepted', 'declined'], default: 'pending' },
+}, {
+  timestamps: true
+});
+
+const Friend = mongoose.model('Friend', friendSchema);
+
+module.exports = Friend;
